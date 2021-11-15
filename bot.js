28bba019e2d2b3fae19763e13522827a3b981c84
Anime-Sama <postmaster@anime-sama.fr>
2021-11-15 11:11:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,23 +1,15 @@
 const fs = require('fs')
 const path = require('path')
-const { Client } = require("discord.js")
 const MonitoRSS = require('monitorss')
 const schedulesPath = path.join(__dirname, 'settings', 'schedules.json')
 const configPath = path.join(__dirname, 'settings', 'config.bot.json')
 const config = fs.existsSync(configPath) ? JSON.parse(fs.readFileSync(configPath)) : {}
 const schedules = fs.existsSync(schedulesPath) ? JSON.parse(fs.readFileSync(schedulesPath)) : {}
 
-const clientdisc = new Client({
-  disableEveryone: true,
-  config
-})
-
 const clientManager = new MonitoRSS.ClientManager({
   setPresence: true,
   schedules,
   config
 })
-
-clientdisc.start()
 
 clientManager.start()
